176cd80357275821e2d75123ea0e5ca2c2c411d8
feiyilin <617694174@qq.com>
2023-07-20 15:11:56 +0800

    demo-java-base模块2

---
+++ demo-java-base/src/main/java/com/xkcoding/java/base/JavaBaseDemoApplication.java
@@ -0,0 +1,28 @@
+package com.xkcoding.java.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * LdapDemoApplication Ldap demo 启动类
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2019-08-26 0:37
+ */
+@SpringBootApplication
+@RestController
+public class JavaBaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseDemoApplication.class, args);
+    }
+
+    @RequestMapping(value="/get",method= RequestMethod.GET)
+    public String get(){
+        return "hello world";
+    }
+}
